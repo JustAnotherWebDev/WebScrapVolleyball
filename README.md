5f38f0a837757c2e8c7096f79ccbeeee2df57c5a
clueless <git@justclueless.bulc.club>
2021-03-30 19:39:17 +0200

    Timer auf 5 Minuten gesetzt und Readme verbessert

--- README.md
+++ README.md
@@ -8,10 +8,20 @@ Guides to follow to setup environment:
 
 # deposite chromedriver in distro:
   sudo wget https://chromedriver.storage.googleapis.com/85.0.4183.87/chromedriver_linux64.zip chrome
+  sudo apt-get install zip unzip
   unzip chrome_driver_linux64.zip
-  cp chromedriver /usr/local/bin/  # is automatically in PATH
+  sudo cp chromedriver /usr/local/bin/  # is automatically in PATH
 
 
 # Why not a WhatsApp-Bot
 #WhatsApp doesn't allow an easy access to sending messages unlinke Telegram, so Telegram > WhatsApp for this and also privacy reasons
+
+
+# Pip install Ubuntu 20.04 LST
+Installation von VirtualEnv  https://itnext.io/how-to-set-up-python-virtual-environment-on-ubuntu-20-04-a2c7a192938d
+Installation unter ZSH! https://gist.github.com/heymonkeyriot/9a2f429caff5c091d5429666fa080403 
+pip install -r requirements.txt
+
+# WSL 2 and Cisco VPN Troubles
+https://gist.github.com/machuu/7663aa653828d81efbc2aaad6e3b1431
 
--- src/app.py
+++ src/app.py
@@ -6,7 +6,8 @@ from sqlalchemy.sql import select, insert
 from bs4 import BeautifulSoup
 import sqlalchemy
 from cup import Cup
-import os, time
+import os
+import time
 import create_database as db
 import telegramBot as tb
 
@@ -19,78 +20,84 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+driver = webdriver.Chrome(
+    executable_path='/usr/local/bin/chromedriver', options=options)
 
 cups_saved = []
 no_cups_found_counter = 0
 s = select([db_cups])
 result = conn.execute(s)
 for row in result:
-  cup_temp = Cup(row['gender'], row['date'], row['category'], row['name'], row['players'], row['link'])
-  cups_saved.append(cup_temp)
+    cup_temp = Cup(row['gender'], row['date'], row['category'],
+                   row['name'], row['players'], row['link'])
+    cups_saved.append(cup_temp)
 
 while True:
-  # Reload the page with webdriver and get cups into soup2 
-  if no_cups_found_counter > 10:
-    tb.send_message_no_cups_found()
+    # Reload the page with webdriver and get cups into soup2
+    if no_cups_found_counter > 10:
+        tb.send_message_no_cups_found()
 
-  driver.get("https://www.beachvolleyball.nrw")
-  try:
-    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,".table-tournaments.table.table-hover")))
-  except selenium.common.exceptions.TimeoutException as e:
-    print("Was not able to retrieve Webpage, will try again in 60 Seonds!")
-    no_cups_found_counter += 1
-    time.sleep(60)
-    break
-  soup1 = BeautifulSoup(driver.page_source, 'lxml')
-  soup2 = soup1.find_all("tr", class_=lambda value: value and value.startswith("series"))
-  cups_found = []
-  # Extract information of each cup
-  try:
-    for cup in soup2:
-      no_cups_found_counter = 0
-      g_gender = cup['class'][0]
-      if g_gender[7] == 'm':
-        a_gender = 'Mixed'
-      elif g_gender[7] == 'h':
-        a_gender = 'Herren'
-      elif g_gender[7] == 'd':
-        a_gender = 'Herren'
-      else:
-        a_gender = 'Senioren'
-      date = cup.find('td', class_="date").get_text()
-      category = cup.find('span', class_="category-shorthandle").get_text()
-      name = cup.find('a').get_text()
-      players = cup.find('td', class_="players").get_text()
-      for l in cup.findAll('a'):
-        link = 'https://www.beachvolleyball.nrw/' + l.get('href')
-      cups_found.append(Cup(a_gender, date, category, name, players, link))
-  except IndexError as e:
-    print(e)
-    print('Cup that caused the IndexError: ', cup)
-    print('Propably someone misstyped when creating a cup!!')
+    driver.get("https://www.beachvolleyball.nrw")
+    try:
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".table-tournaments.table.table-hover")))
+    except selenium.common.exceptions.TimeoutException as e:
+        print("Was not able to retrieve Webpage, will try again in 5 Minutes!")
+        no_cups_found_counter += 1
+        time.sleep(300)
+        break
+    soup1 = BeautifulSoup(driver.page_source, 'lxml')
+    soup2 = soup1.find_all(
+        "tr", class_=lambda value: value and value.startswith("series"))
+    cups_found = []
+    # Extract information of each cup
+    try:
+        for cup in soup2:
+            no_cups_found_counter = 0
+            g_gender = cup['class'][0]
+            if g_gender[7] == 'm':
+                a_gender = 'Mixed'
+            elif g_gender[7] == 'h':
+                a_gender = 'Herren'
+            elif g_gender[7] == 'd':
+                a_gender = 'Herren'
+            else:
+                a_gender = 'Senioren'
+            date = cup.find('td', class_="date").get_text()
+            category = cup.find(
+                'span', class_="category-shorthandle").get_text()
+            name = cup.find('a').get_text()
+            players = cup.find('td', class_="players").get_text()
+            for l in cup.findAll('a'):
+                link = 'https://www.beachvolleyball.nrw/' + l.get('href')
+            cups_found.append(
+                Cup(a_gender, date, category, name, players, link))
+    except IndexError as e:
+        print(e)
+        print('Cup that caused the IndexError: ', cup)
+        print('Propably someone misstyped when creating a cup!!')
 
-  #Compare the found cups with the cups already found and saved in cups_saved
-  cup_found = False
-  for cup in cups_found:
-    if cup not in cups_saved:
-      cup_found = True
-      print('FOUND NEW CUP!')
-      #Add cup to the saved list
-      cups_saved.append(cup)
-      
-      # insert() expects lmdb but is not neccessary, problem between sqlalchemy and pylint
-      ins = db_cups.insert().values(id=cup.id, gender=cup.gender, date=cup.date, category=cup.category, name=cup.name, players=cup.players, link=cup.link)
-      try:
-        result = conn.execute(ins)
-      except sqlalchemy.exc.IntegrityError as e:
-          print()
-          print(e)
-          print(cup)
-          print()
-      tb.send_message(f'{cup.link} \nNeuer {cup.gender}-Cup in {cup.name} am {cup.date} \nAngemeldet sind : {cup.players} Teams \n')
-  if cup_found == False:
-    print('Was not able to find new cups!')
-  time.sleep(60)
+    # Compare the found cups with the cups already found and saved in cups_saved
+    cup_found = False
+    for cup in cups_found:
+        if cup not in cups_saved:
+            cup_found = True
+            print('FOUND NEW CUP!')
+            # Add cup to the saved list
+            cups_saved.append(cup)
 
-# end of program
+            # insert() expects lmdb but is not neccessary, problem between sqlalchemy and pylint
+            ins = db_cups.insert().values(id=cup.id, gender=cup.gender, date=cup.date,
+                                          category=cup.category, name=cup.name, players=cup.players, link=cup.link)
+            try:
+                result = conn.execute(ins)
+            except sqlalchemy.exc.IntegrityError as e:
+                print()
+                print(e)
+                print(cup)
+                print()
+            tb.send_message(
+                f'{cup.link} \nNeuer {cup.gender}-Cup in {cup.name} am {cup.date} \nAngemeldet sind : {cup.players} Teams \n')
+    if cup_found == False:
+        print('Was not able to find new cups! Will try again in 5 Minutes')
+    time.sleep(300)
